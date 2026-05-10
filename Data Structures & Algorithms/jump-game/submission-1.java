0ef48d67b7eb30dad4ed7ba89d7ae7b06b41560e
Ksenia <ksenija.schulz@gmail.com>
2026-05-10 15:29:41 +0200

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.java
@@ -0,0 +1,43 @@
+class Solution {
+    // public boolean canJump(int[] nums) {
+    //     int[] cache = new int[nums.length];
+    //     Arrays.fill(cache, -1);
+    //     return memoization(nums, 0, cache);        
+    // }
+
+    // public boolean memoization(int[] nums, int i, int[] cache) {
+    //     if (i >= nums.length - 1) {
+    //         return true;
+    //     }
+
+    //     if (cache[i] != -1) {
+    //         return cache[i] == 1;
+    //     }
+
+    //     cache[i] = 0;
+
+    //     cache[i] = memoization(nums, i + nums[i], cache) ? 1 : 0;
+    //     if (cache[i] == 1) {
+    //         return true;
+    //     }
+
+    //     for (int j = nums[i] - 1; j > 0; j--) {
+    //         cache[i] = memoization(nums, i + j, cache) ? 1 : 0;
+    //         if (cache[i] == 1) {
+    //             return true;
+    //         }
+    //     }
+
+    //     return cache[i] == 1;
+    // }
+
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        } 
+        return goal == 0;
+    }
+}
