dc9d459459764b386234f22f83f48d7c3fdbefec
Ksenia <ksenija.schulz@gmail.com>
2026-06-02 15:38:12 +0200

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res = res << 1;
+            if ((n & 1) == 1) {
+                res++;
+            }   
+            n = n >> 1;
+        }  
+        return res;   
+    }
+}
