afbcb853504fc0b322c7416416179be4c3093a67
Ksenia <ksenija.schulz@gmail.com>
2026-07-22 19:59:19 +0200

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>(); // (neighbor, time)
+        for (int i = 1; i <= n; i++) {
+            adj.put(i, new ArrayList<>());
+        }
+        for (int[] edge : times) {
+            int ui = edge[0], vi = edge[1], ti = edge[2];
+            adj.get(ui).add(new int[]{vi, ti});
+        }
+
+        Map<Integer, Integer> shortest = new HashMap<>(); // {node, time}
+        Queue<int[]> minHeap = new PriorityQueue<>((n1, n2) -> n1[0] - n2[0]); // (total time, node)
+        minHeap.add(new int[]{0, k});
+        
+        while(!minHeap.isEmpty()) {
+            int[] cur = minHeap.remove();
+            int t1 = cur[0], n1 = cur[1];
+
+            if (shortest.containsKey(n1)) {
+                continue;
+            }
+            shortest.put(n1, t1);
+
+            if (shortest.size() == n) {
+                return t1;
+            }
+
+            for (int[] neighbor : adj.get(n1)) {
+                int n2 = neighbor[0], t2 = neighbor[1];
+                if (!shortest.containsKey(n2)) {
+                    minHeap.add(new int[]{t1 + t2, n2});
+                }
+            }
+        }  
+        return -1;
+    }
+}
